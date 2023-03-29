66eacb4d0d4fde93a92c2ebcc0059bfb74bb922d
JayBuckleyUpdox <73846360+JayBuckleyUpdox@users.noreply.github.com>
2023-03-29 13:03:47 -0400

    readme

--- xJinx.lua
+++ xJinx.lua
@@ -1,47 +1,4 @@
--- xJinx by Jay and a bit of ampx.
 
--- TODO
--- improve the dash detection logic using db
--- AutoKS
--- -- w reimplement 
-
--- damage visualizer 
--- -- fix bar size
--- -- add crit chance to damage calc
-
--- Farming 
--- -- add clear and fast clear logic
--- Combo
--- -- investigte more options
-
--- -=--==-=-=-=-=-
--- Features
-	-- Combo Q swap
-  -- Combo W 
-
-  -- harass 
-  -- -- harass Q swap
-  -- -- -- optional Extend AutoAttack range with minion splash radius
-  -- -- harass W
-
-  -- Auto Chain CC/immobile/channel/Dash traps (prediction / timing based)
-  -- Auto Chain CC/immobile/channel/Dash laser (prediction / timing based)
-
-  -- semiAutoR (while holding U)
-  -- SemiAutoW (Full combo)
-
-  -- Auto KS with R
-
-  -- Damage visualizer
-  -- -- HP bar shows damage of 3 autos + W and R if available or 4 autos if attack speed is above 1.5
-  -- -- "Combo Kill" text shows if hp below W+R
-  -- -- "W Kill" text shows if hp below W
-  -- -- "AA Kill" shows if hp below AA damage 
-
-  -- Harass visualizer
-  -- Draws circle around target who could be safely hit by attacking a minion when otherwise impossible to reach
-
-  -- Debug toggle [Shows all debug message on screen]
 
 
 local core = require("xCore") 
